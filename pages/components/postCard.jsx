554681ad57443a2a9cd7697081c717300b74f765
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-12-07 16:34:20 +0700

    fix

--- pages/components/postCard.jsx
+++ pages/components/postCard.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Box } from "@chakra-ui/react";
 
 function PostCard({ title, date, description, slug }) {
   return (
@@ -8,7 +9,32 @@ function PostCard({ title, date, description, slug }) {
       <p className="mt-4 italic">{description}</p>
 
       <Link href="/[slug]" as={`/${slug}`}>
-        Read More
+        <Box
+          as="button"
+          height="24px"
+          lineHeight="1.2"
+          transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
+          border="1px"
+          px="8px"
+          borderRadius="2px"
+          fontSize="14px"
+          fontWeight="semibold"
+          bg="#f5f6f7"
+          borderColor="#ccd0d5"
+          color="#4b4f56"
+          _hover={{ bg: "#ebedf0" }}
+          _active={{
+            bg: "#dddfe2",
+            transform: "scale(0.98)",
+            borderColor: "#bec3c9",
+          }}
+          _focus={{
+            boxShadow:
+              "0 0 1px 2px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)",
+          }}
+        >
+          Baca Sekarang
+        </Box>
       </Link>
     </div>
   );
